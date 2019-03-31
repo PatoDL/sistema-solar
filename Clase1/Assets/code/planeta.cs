0c550c0d339ede8690527393be3643ec50d3755e
Pato <patodl2701@gmail.com>
2019-03-31 18:31:51 -0300

    trail renderer added

--- Clase1/Assets/code/planeta.cs
+++ Clase1/Assets/code/planeta.cs
@@ -13,6 +13,10 @@ public class planeta : MonoBehaviour
 
     public GameObject sol;
 
+    const int axis = 3;
+
+   float[] anguloRotacion = new float [axis];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,6 +25,11 @@ public class planeta : MonoBehaviour
         transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
 
         actualPos = Random.Range(-180.0f, 180.0f);
+
+        for(int i=0;i<anguloRotacion.Length;i++)
+        {
+            anguloRotacion[i] = 0;
+        }
     }
 
     // Update is called once per frame
@@ -31,5 +40,15 @@ public class planeta : MonoBehaviour
         transform.position = new Vector3(newx, transform.position.y, newz);
 
         actualPos += Time.deltaTime * speed;
+
+        transform.Rotate(new Vector3(anguloRotacion[0], anguloRotacion[1], anguloRotacion[2]));
+
+        for (int i = 0; i < anguloRotacion.Length; i++)
+        {
+            if (anguloRotacion[i] < 1)
+            {
+                anguloRotacion[i] += Time.deltaTime;
+            }
+        }
     }
 }
