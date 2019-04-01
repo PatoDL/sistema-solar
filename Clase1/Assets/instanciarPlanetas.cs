9c3810b7bcf8b9275d9d8063fe0092fe41d4be8b
Pato <patodl2701@gmail.com>
2019-04-01 10:47:48 -0300

    another scene was added with new prefab planets

---
+++ Clase1/Assets/instanciarPlanetas.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class instanciarPlanetas : MonoBehaviour
+{
+    public Transform planeta;
+    public Transform sol;
+    const int cantPlanetas = 8;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(planeta)
+        {
+            for(int i=0;i<cantPlanetas; i++)
+            {
+                Transform p = Instantiate(planeta);
+                p.GetComponent<pfPlaneta>().radio = i * 10;
+                p.transform.position = new Vector3(sol.transform.position.x, sol.transform.position.y,
+                    sol.transform.position.z + p.GetComponent<pfPlaneta>().radio);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
